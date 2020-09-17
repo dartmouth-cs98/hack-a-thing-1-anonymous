03efeee1a3e41dd9713bc54d3e7373fd03270b11
ggualtieri <grant.s.gualtieri.21@dartmouth.edu>
2020-09-16 21:31:18 -0400

    pushing project for the first time

---
+++ HackathonOne/HackathonOneTests/HackathonOneTests.swift
@@ -0,0 +1,34 @@
+//
+//  HackathonOneTests.swift
+//  HackathonOneTests
+//
+//  Created by Grant Gualtieri on 9/16/20.
+//  Copyright © 2020 Anonymous. All rights reserved.
+//
+
+import XCTest
+@testable import HackathonOne
+
+class HackathonOneTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
